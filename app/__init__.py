67ac355b6342d02d16f16bfa08e4f396602eb65c
Nicholas A. Del Grosso <delgrosso@bio.lmu>
2016-06-04 23:09:27 +0200

    Generate links to some rendered notebooks using a template.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, send_from_directory
 from flask_bootstrap import Bootstrap
+import os
+from os import path
+from glob import glob
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 Bootstrap(app)
 
+def count_total_lectures():
+    staturl = glob(path.join(app.static_folder, 'lecture*'))
+    return len(staturl)
+
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    tot_lectures = count_total_lectures()
+    return render_template('index.html', tot_lectures=tot_lectures)
+
+@app.route('/course/<int:lecnum>')
+def lecture(lecnum):
+    return send_from_directory('static',
+                               filename='lecture{}.html'.format(lecnum))
