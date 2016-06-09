ce9ec9f01c420a46b1d6a6c0afb1f3f37d35e255
Nicholas A. Del Grosso <delgrosso@bio.lmu>
2016-06-09 18:17:35 +0200

    Moved route code to the views.py module

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template, url_for, send_from_directory, redirect
+from flask import Flask
 from flask_bootstrap import Bootstrap
-import os
-from os import path
-from glob import glob
-
 
 # Setup App object
 app = Flask(__name__)
@@ -11,26 +7,4 @@ app.config.from_object('config')
 
 Bootstrap(app)
 
-
-slide_dir = path.join(app.static_folder, 'pycourse', 'lessons')
-slides = [glob(path.join(slide_dir, dd, '*.slides.html'))[0] for dd in os.listdir(slide_dir)]
-slides = sorted(slides)
-
-
-# Routing
-@app.route('/')
-def hello_world():
-    slide_names = [path.dirname(slide).split('/')[-1].split('_')[1] for slide in slides]
-    print(slide_names)
-    return render_template('index.html', slides=slide_names)
-
-@app.route('/<int:lecnum>')
-def lecture(lecnum):
-    return send_from_directory(path.dirname(slides[lecnum-1]),
-                               filename=path.basename(slides[lecnum-1]))
-
-
-
-@app.route('/format')
-def code_format_test():
-    return render_template('base.html')
+from app import views
